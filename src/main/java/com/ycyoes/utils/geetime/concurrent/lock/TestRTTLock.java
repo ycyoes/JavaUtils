76736679b160ad1c0ea76adddf02206afcd86c88
ycyoes <ycyoes@xiezhaoangdembp>
2019-10-27 20:51:59 +0800

    可重入锁

---
+++ src/main/java/com/ycyoes/utils/geetime/concurrent/lock/TestRTTLock.java
@@ -0,0 +1,34 @@
+package com.ycyoes.utils.geetime.concurrent.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class TestRTTLock {
+	private double x, y;
+	private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+	//读锁
+	private Lock readLock = lock.readLock();
+	//写锁
+	private Lock writeLock = lock.writeLock();
+	public double read() {
+		//获取读锁
+		readLock.lock();
+		try {
+			return Math.sqrt(x * x + y * y);
+		} finally {
+			//释放读锁
+			readLock.unlock();
+		}
+	}
+	
+	public void move(double deltaX, double deltaY) {
+		//获取写锁
+		writeLock.lock();
+		try {
+			x += deltaX;
+			y += deltaY;
+		} finally {
+			writeLock.unlock();
+		}
+	}
+}
