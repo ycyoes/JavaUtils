ced60bacd081ab81ecc773f788d0e65ac29eb5c6
ycyoes <ycyoes@163.com>
2019-12-15 13:56:16 +0800

    不正确的访问资源

---
+++ src/main/java/com/ycyoes/demos/thinkinginjava/concurrency/access/EvenGenerator.java
@@ -0,0 +1,15 @@
+package com.ycyoes.demos.thinkinginjava.concurrency.access;
+
+//When threads collide.
+public class EvenGenerator extends IntGenerator{
+    private int currentEvenValue = 0;
+    public int next() {
+        ++currentEvenValue; //Danger point here!
+        ++currentEvenValue;
+        return currentEvenValue;
+    }
+
+    public static void main(String[] args) {
+        EvenChecker.test(new EvenGenerator());
+    }
+}
