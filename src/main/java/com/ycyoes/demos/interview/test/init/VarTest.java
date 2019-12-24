7c3f78af0d6aade7a037f689f60b2c2104563743
ycyoes <ycyoes@163.com>
2019-12-24 16:02:30 +0800

    变量初始化

---
+++ src/main/java/com/ycyoes/demos/interview/test/init/VarTest.java
@@ -0,0 +1,20 @@
+package com.ycyoes.demos.interview.test.init;
+
+public class VarTest {
+
+    public static void main(String[] args) {
+        modify();
+    }
+    public static void modify(){
+        int i, j = 1, k = 0;
+//        int i, j, k;    //j,k未初始化，报错
+        i = 100;
+        while(i > 0) {
+            i = j * 2;
+            System.out.println("The value of j is: " + j);
+            k = k + 1;
+            i--;
+        }
+    }
+
+}
