23c4f4454ba3494150ea1b0220e67ae89194a693
ycyoes <ycyoes@163.com>
2019-12-28 18:52:51 +0800

    数组转List集合后，适于只读

---
+++ src/main/java/com/ycyoes/demos/aliyun/mldn/test/collection/ArrayAsList.java
@@ -0,0 +1,17 @@
+package com.ycyoes.demos.aliyun.mldn.test.collection;
+
+import java.util.Arrays;
+import java.util.List;
+//数组转集合后，再进行add/remove/clear操作会报错：java.lang.UnsupportedOperationException
+public class ArrayAsList {
+    public static void main(String[] args) {
+        String[] stringArray = new String[3];
+        stringArray[0] = "one";
+        stringArray[1] = "two";
+        stringArray[2] = "three";
+        List<String> list = Arrays.asList(stringArray);
+        System.out.println(list);
+        list.add("four");
+        System.out.println(list);
+    }
+}
