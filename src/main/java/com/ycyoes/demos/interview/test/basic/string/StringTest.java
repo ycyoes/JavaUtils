254bd8648865ddba0a04e321057e3f342cdd83ae
ycyoes <ycyoes@163.com>
2020-01-18 22:10:32 +0800

    toArray(T[] a)

--- src/main/java/com/ycyoes/demos/interview/test/basic/string/StringTest.java
+++ src/main/java/com/ycyoes/demos/interview/test/basic/string/StringTest.java
@@ -1,5 +1,10 @@
 package com.ycyoes.demos.interview.test.basic.string;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class StringTest {
 	public static void main(String[] args) {
 		//因为他们比较的不是对象中字段的值或者说本身对象的值，比较的是物理地址，所以结果都为false
@@ -9,5 +14,19 @@ public class StringTest {
 		System.out.println(obj == obj1);
 		System.out.println(obj.toString());
 		System.out.println(obj1.toString());
+		String xmlInput = "hello:world";
+		int key = xmlInput.indexOf(":");
+		System.out.println(key);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("1", "first");
+		map.put("2", "second");
+		map.put("3", "third");
+		String[] keys = map.keySet().toArray(new String[map.size()]);
+		System.out.println(Arrays.asList(keys));
+		
+		Map<String, List<String>> mapList = new HashMap<String, List<String>>();
+		List<String> list = mapList.get("4");
+		list.add("hello");
+		System.out.println(list);
 	}
 }
