6902a17260b2902135758bd24e8b90198621e526
ycyoes <ycyoes@163.com>
2020-01-28 16:40:26 +0800

    并发和串行执行比较

---
+++ src/main/java/com/ycyoes/demos/concurrency/art/first/ConcurrencyTest.java
@@ -0,0 +1,46 @@
+package com.ycyoes.demos.concurrency.art.first;
+
+public class ConcurrencyTest {
+    private static final long count = 100001;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (int i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (int i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serail: " + time + "ms, b = " + b + ", a = " + a);
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (int i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (int i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency: " + time + "ms, b=" + b);
+    }
+
+}
