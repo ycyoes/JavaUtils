8d50dcbf56a3947a6dfa62ac00f315d13bd5f05e
ycyoes <ycyoes@163.com>
2020-02-01 20:58:22 +0800

    Daemon线程

---
+++ src/main/java/com/ycyoes/demos/concurrency/art/forth/Daemon.java
@@ -0,0 +1,28 @@
+package com.ycyoes.demos.concurrency.art.forth;
+
+import com.ycyoes.demos.concurrency.art.utils.SleepUtils;
+
+/**
+ * Daemon线程
+ * 在构建Daemon线程时，不能依靠finally块中的内容来确保执行关闭或清理资源的逻辑
+ * 
+ * @author ycyoes
+ *
+ */
+public class Daemon {
+	public static void main(String[] args) {
+		Thread thread = new Thread(new DaemonRunner(), "DaemonRunner");
+		thread.setDaemon(true);
+		thread.start();
+	}
+	
+	static class DaemonRunner implements Runnable {
+		public void run() {
+			try {
+				SleepUtils.second(10);
+			} finally {
+				System.out.println("DaemonThread finally run.");
+			}
+		}
+	}
+}
