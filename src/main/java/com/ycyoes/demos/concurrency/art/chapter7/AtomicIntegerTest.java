d0bdba63ebee3b19fec0ea426de8b00a126bf417
ycyoes <ycyoes@163.com>
2020-02-02 21:42:40 +0800

    AtomicInteger

---
+++ src/main/java/com/ycyoes/demos/concurrency/art/chapter7/AtomicIntegerTest.java
@@ -0,0 +1,11 @@
+package com.ycyoes.demos.concurrency.art.chapter7;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest {
+	static AtomicInteger ai = new AtomicInteger(1);
+	public static void main(String[] args) {
+		System.out.println(ai.getAndIncrement());
+		System.out.println(ai.get());
+	}
+}
