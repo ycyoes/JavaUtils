3579288c9f7b5025987a9a92a84b6fe58f9b3563
ycyoes <ycyoes@163.com>
2020-02-03 21:14:55 +0800

    join

--- src/main/java/com/ycyoes/demos/concurrency/art/chapter8/JoinCountDownLatchTest.java
+++ src/main/java/com/ycyoes/demos/concurrency/art/chapter8/JoinCountDownLatchTest.java
@@ -27,6 +27,8 @@ public class JoinCountDownLatchTest {
 		parser1.start();
 		parser2.start();
 		
+		//join用于让当前执行线程等待join线程执行结束。其实现原理是不停检查join线程是否存活，
+		//如果join线程存活则让当前线程永远等待。其中，wait(0)表示永远等待下去。
 		parser1.join();
 		parser2.join();
 		System.out.println("all parser finish");
