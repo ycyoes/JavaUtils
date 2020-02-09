c0c476b66d98837f139d4e9f816f7e2600aa8d98
ycyoes <ycyoes@163.com>
2020-02-09 15:12:59 +0800

    UnsafeLazyInitialization

---
+++ src/main/java/com/ycyoes/demos/concurrency/art/chapter3/doublecheck/UnsafeLazyInitialization.java
@@ -0,0 +1,14 @@
+package com.ycyoes.demos.concurrency.art.chapter3.doublecheck;
+
+public class UnsafeLazyInitialization {
+	private static Instance instance;
+	
+	public static Instance getInstance() {
+		if (instance == null) {			//1: A线程执行
+			instance = new Instance();	//2: B线程执行
+		}
+		return instance;
+	}
+	
+	static class Instance {}
+}
