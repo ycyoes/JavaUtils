db7848e235779f976d2ceee187c54163eabbea37
ycyoes <ycyoes@163.com>
2020-03-08 20:26:49 +0800

    组装类名

--- src/main/java/com/ycyoes/demos/basic/reflection/utils/ReflectUtils.java
+++ src/main/java/com/ycyoes/demos/basic/reflection/utils/ReflectUtils.java
@@ -2,7 +2,11 @@ package com.ycyoes.demos.basic.reflection.utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.SortedSet;
+import java.util.stream.Collectors;
 
 public class ReflectUtils {
 	
@@ -10,6 +14,12 @@ public class ReflectUtils {
 		List<String> list = new ArrayList<String>();
 		System.out.println(isCollection(List.class));
 		System.out.println(isCollection(list.getClass()));
+		
+		List<Class<?>> cls = new ArrayList<Class<?>>();
+		cls.add(Object.class);
+		cls.add(List.class);
+		cls.add(SortedSet.class);
+		System.out.println(getTypes(cls));
 	}
 
 	/**
@@ -22,4 +32,10 @@ public class ReflectUtils {
 	public static <T> boolean isCollection(Class<T> type) {
 	    return Collection.class.isAssignableFrom(type);
 	  }
+	
+	//form class name - 组装类名
+	public static String getTypes(List<Class<?>> list) {
+		return Optional.ofNullable(list).orElseGet(Collections::emptyList)
+		          .stream().map(Class::getSimpleName).collect(Collectors.joining(","));
+	}
 }
