d3cac556a1657a35715fa7db038cbcf92c54d78f
ycyoes <ycyoes@163.com>
2020-03-22 17:10:14 +0800

    XX:MaxTenuringThreshold

--- src/main/java/com/ycyoes/demos/jvm/feature/chapter3/MemoryAllocation.java
+++ src/main/java/com/ycyoes/demos/jvm/feature/chapter3/MemoryAllocation.java
@@ -1,11 +1,12 @@
-//package com.ycyoes.demos.jvm.feature.chapter3;
+package com.ycyoes.demos.jvm.feature.chapter3;
 
 public class MemoryAllocation {
 	private static final int _1MB = 1024 * 1024;
 	
 	public static void main(String[] args) {
 //		testAllocation();
-		testPretenureSizeThreshold();
+//		testPretenureSizeThreshold();
+		testTenuringThreshold();
 	}
 	
 	/**
@@ -26,5 +27,19 @@ public class MemoryAllocation {
 	public static void testPretenureSizeThreshold() {
 		byte[] allocation;
 		allocation = new byte[4 * _1MB];	//直接分配在老年代中
+	}
+	
+	/**
+	 * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+	 * -XX:MaxTenuringThreshold=1
+	 * -XX:+PrintTenuringDistribution
+	 */
+	public static void testTenuringThreshold() {
+		byte[] allocation1, allocation2, allocation3;
+		allocation1 = new byte[_1MB / 4];	//什么时候进入老年代决定于XX:MaxTenuringThreshold设置
+		allocation2 = new byte[4 * _1MB];
+		allocation3 = new byte[4 * _1MB];
+		allocation3 = null;
+		allocation3 = new byte[4 * _1MB];
 	}
 }
