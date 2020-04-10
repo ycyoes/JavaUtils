8b742b4b658e025563ea91121fbe89e394405722
yechaoyang <ycyoes@163.com>
2020-04-10 14:06:21 +0800

    lock.lock()需放到try-catch块外

--- src/main/java/com/ycyoes/demos/test/lock/LockTest.java
+++ src/main/java/com/ycyoes/demos/test/lock/LockTest.java
@@ -11,8 +11,49 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class LockTest {
     final static Lock lock = new ReentrantLock();
+    private int count = 0;
+    public Runnable run1 = new Runnable() {
+        public void run() {
+            //加锁
+            lock.lock();
+            while (count < 1000) {
+                try {
+                    //打印是否执行该方法
+                    System.out.println(Thread.currentThread().getName() + "run1:" + count++);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            lock.unlock();
+        }
+    };
+    //方法2
+    public Runnable run2 = new Runnable() {
+        public void run() {
+            lock.lock();
+            while(count<1000){
+                try {
+                    System.out.println(Thread.currentThread().getName()+"run2"+count++);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            lock.unlock();
+        }
+    };
 
     public static void main(String[] args) {
+//        lockTry();
+        //创建一个对象
+        LockTest s = new LockTest();
+        //获取该对象的方法1
+        new Thread(s.run1).start();
+        //获取该对象的方法2
+        new Thread(s.run2).start();
+
+    }
+
+    private static void lockTry() {
         lock.lock();
         System.out.println("lock.");
         try {
@@ -24,6 +65,5 @@ public class LockTest {
             System.out.println("release lock!");
             lock.unlock();
         }
-
     }
 }
