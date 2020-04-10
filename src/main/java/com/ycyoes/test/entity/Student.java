889e37624357edc3bceee20b786c014b24874286
yechaoyang <ycyoes@163.com>
2020-04-10 14:29:09 +0800

    新增实体

---
+++ src/main/java/com/ycyoes/test/entity/Student.java
@@ -0,0 +1,26 @@
+package com.ycyoes.test.entity;
+
+/**
+ * 实体类
+ *
+ */
+public class Student {
+    private Long age;
+    private String name;
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
