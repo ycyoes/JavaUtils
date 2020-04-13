1b81b6f0831ffb2d242729429b943668e5848cc5
yechaoyang <ycyoes@163.com>
2020-04-13 17:24:29 +0800

    ResultSet设值到实体

--- src/main/java/com/ycyoes/demos/basic/reflection/utils/ReflectUtils.java
+++ src/main/java/com/ycyoes/demos/basic/reflection/utils/ReflectUtils.java
@@ -1,15 +1,15 @@
 package com.ycyoes.demos.basic.reflection.utils;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.SortedSet;
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import com.ycyoes.demos.basic.reflection.RichType;
+
+import static com.ycyoes.utils.StringUtils.lineToHump;
 
 public class ReflectUtils {
 	
@@ -43,5 +43,57 @@ public class ReflectUtils {
 	public static String getTypes(List<Class<?>> list) {
 		return Optional.ofNullable(list).orElseGet(Collections::emptyList)
 		          .stream().map(Class::getSimpleName).collect(Collectors.joining(","));
+	}
+
+	/**
+	 * ResultSet设值到实体
+	 *
+	 * @param rs
+	 * @param clz
+	 * @param <T>
+	 * @return
+	 * @throws Exception
+	 */
+	public static <T> List<T> resultSetToEntity(ResultSet rs, Class<T> clz) throws Exception{
+		if (rs == null) {
+			return Collections.EMPTY_LIST;
+		}
+		List<T> list = new ArrayList<>();
+		//获取结果集元数据
+		ResultSetMetaData rsmeta = rs.getMetaData();
+		Map<String, Class> map = new HashMap<>();
+		Arrays.stream(clz.getDeclaredFields()).forEach((fs) -> {
+			map.put(fs.getName(), fs.getType());
+		});
+
+		while(rs.next()) {
+			T t = clz.getConstructor().newInstance();
+			//获取结果集中字段数
+			int count = rsmeta.getColumnCount();
+			//循环取出字段名，并将元数据中数值赋值给对应实体对象属性
+			for(int i = 0; i < count; i++) {
+				//获取字段名
+				String rsName = rsmeta.getColumnName(i + 1);
+				String name = rsName.indexOf("_") > -1 ? lineToHump(rsName) : rsName;
+				if (map.containsKey(name)) {
+					String setName = "set" + name.toUpperCase().substring(0, 1) + name.substring(1);
+					Class<?> type = map.get(name);
+					Method setMethod = clz.getMethod(setName, type);
+					//数据库中decimal类型，ResultSet中为Unknown MySQL Type,调用实体set方法设值报错：argument type mismatch
+					//单独匹配进行设值
+					switch (type.getName()) {
+						case "java.lang.Long":
+							setMethod.invoke(t, rs.getLong(rsName));
+							break;
+						case "java.lang.Short":
+							setMethod.invoke(t, rs.getShort(rsName));
+							break;
+						default: setMethod.invoke(t, rs.getObject(rsName));
+					}
+				}
+			}
+			list.add(t);
+		}
+		return list;
 	}
 }
