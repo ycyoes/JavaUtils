7edfc0fed889588801751286d7bee034d7ede9c9
yechaoyang <ycyoes@163.com>
2020-05-11 16:50:21 +0800

    StringUtils类

---
+++ src/main/java/com/ycyoes/utils/test/StringUtils.java
@@ -0,0 +1,92 @@
+package com.ycyoes.utils.test;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+    private static Pattern humpPattern = Pattern.compile("[A-Z]");
+    private static Pattern linePattern = Pattern.compile("_(\\w)");
+    private static Random random = new Random();
+
+    public static String lineToHump(String str) {
+        str = str.toLowerCase();
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    public static String humpToLine(String str) {
+        Matcher matcher = humpPattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    public static String getUuid() {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        return uuid + random.nextInt(10) + random.nextInt(10);
+    }
+
+    public static void main(String[] args) {
+        String uuid = getUuid();
+        System.out.println(uuid);
+    }
+
+    public static String underlineToCamel(String line, boolean smallCamel) {
+        if (line != null && !"".equals(line)) {
+            StringBuffer sb = new StringBuffer();
+            Pattern pattern = Pattern.compile("([A-Za-z\\d]+)(_)?");
+            Matcher matcher = pattern.matcher(line);
+
+            while(matcher.find()) {
+                String word = matcher.group();
+                sb.append(smallCamel && matcher.start() == 0 ? Character.toLowerCase(word.charAt(0)) : Character.toUpperCase(word.charAt(0)));
+                int index = word.lastIndexOf(95);
+                if (index > 0) {
+                    sb.append(word.substring(1, index).toLowerCase());
+                } else {
+                    sb.append(word.substring(1).toLowerCase());
+                }
+            }
+
+            return sb.substring(0, 1).toUpperCase() + sb.substring(1);
+        } else {
+            return "";
+        }
+    }
+
+    public static String camelToUnderline(String line) {
+        if (line != null && !"".equals(line)) {
+            line = String.valueOf(line.charAt(0)).toUpperCase().concat(line.substring(1));
+            StringBuffer sb = new StringBuffer();
+            Pattern pattern = Pattern.compile("[A-Z]([a-z\\d]+)?");
+            Matcher matcher = pattern.matcher(line);
+
+            while(matcher.find()) {
+                String word = matcher.group();
+                sb.append(word.toUpperCase());
+                sb.append(matcher.end() == line.length() ? "" : "_");
+            }
+
+            return sb.toString();
+        } else {
+            return "";
+        }
+    }
+    
+    public static boolean equalTo(Object a, Object b) {
+    	return Objects.equals(a, b);
+    }
+
+}
