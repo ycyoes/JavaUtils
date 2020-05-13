6f787933751e9c322e16536ab04b2c5223fe0e83
yechaoyang <ycyoes@163.com>
2020-05-13 10:22:24 +0800

    布隆过滤器示例

--- src/main/java/com/ycyoes/test/concurrency/ThreadLocalDemo.java
+++ src/main/java/com/ycyoes/test/concurrency/ThreadLocalDemo.java
@@ -6,6 +6,8 @@ public class ThreadLocalDemo {
     public static void main(String[] args) {
         Thread thread = Thread.currentThread();
         ThreadLocal threadLocal = new ThreadLocal();
-        threadLocal.set("test");
+        threadLocal.set("test测试");
+        System.out.println(threadLocal.get());
+
     }
 }
