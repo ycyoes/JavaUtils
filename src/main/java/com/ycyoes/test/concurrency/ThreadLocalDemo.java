5e13a10c06314f7c85fae9773cfd32fbcb1084e2
yechaoyang <ycyoes@163.com>
2020-05-13 09:22:26 +0800

    添加guava依赖

---
+++ src/main/java/com/ycyoes/test/concurrency/ThreadLocalDemo.java
@@ -0,0 +1,11 @@
+package com.ycyoes.test.concurrency;
+
+import com.sun.jmx.snmp.ThreadContext;
+
+public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        ThreadLocal threadLocal = new ThreadLocal();
+        threadLocal.set("test");
+    }
+}
