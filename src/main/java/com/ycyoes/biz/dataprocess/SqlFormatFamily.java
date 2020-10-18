77323d846ebf2727c90eac6595b19b30a63c4a8a
ycyoes <ycyoes@163.com>
2020-10-18 18:54:20 +0800

    导入户主关系-格式化SQL

--- src/main/java/com/ycyoes/biz/dataprocess/SqlFormatFamily.java
+++ src/main/java/com/ycyoes/biz/dataprocess/SqlFormatFamily.java
@@ -3,28 +3,36 @@ package com.ycyoes.biz.dataprocess;
 import com.ycyoes.common.io.FileUtils;
 import com.ycyoes.utils.StringUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.ycyoes.demos.test.text.HandleText.readFile;
 
 public class SqlFormatFamily {
     public static void main(String[] args) {
         String name = "D:/huzhu.txt";
-        System.out.println(name);
         List<String> list = readFile(name);
-        String head = "insert into ZZ_FAMILY_202010171648(id, p_id, create_date, create_user) select sys_guid(), (select id from zz_person where card_num='";
+        String head = "insert into ZZ_FAMILY_202010181418(id, p_id, create_date, create_user) select sys_guid(), (select id from zz_person where card_num='";
         String tail = "'),sysdate, 'system' from dual;";
         StringBuilder sb = new StringBuilder();
+        Map<String, Integer> map = new HashMap();
         for (int i = 0; i < list.size(); i++) {
             String cardNum = list.get(i);
             if (StringUtils.isNotBlank(cardNum)) {
                 int len = cardNum.length();
-                if (len < 17) {
+                if (len < 18) {
                     System.out.println("长度: " + len + " 身份证长度不符 " + cardNum);
                 }
-                sb.append(head + list.get(i) + tail + "\n");
+                map.put(cardNum, i);
             }
         }
-        FileUtils.writeToFile("D:/sql.txt", sb.toString(), false);
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            sb.append(head + entry.getKey() + tail + "\n");
+            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+//        System.out.println(sb.toString());
+        FileUtils.writeToFile("D:/sql1.txt", sb.toString(), false);
     }
+
 }
