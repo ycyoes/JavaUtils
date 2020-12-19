acde7aba1681a99ebbf7f01519f1b6819aa7c018
ycyoes <ycyoes@163.com>
2020-12-19 17:05:57 +0800

    test

--- src/main/java/com/ycyoes/biz/dataprocess/Test.java
+++ src/main/java/com/ycyoes/biz/dataprocess/Test.java
@@ -1,5 +1,7 @@
 package com.ycyoes.biz.dataprocess;
 
 public class Test {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
