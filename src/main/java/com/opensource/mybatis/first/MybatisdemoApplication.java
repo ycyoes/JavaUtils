b7be847cfa3e53a4a8858c71f924f5e851e6b2cb
ycyoes <ycyoes@163.com>
2021-06-22 22:34:56 +0800

    启动类

---
+++ src/main/java/com/opensource/mybatis/first/MybatisdemoApplication.java
@@ -0,0 +1,15 @@
+package com.opensource.mybatis.first;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisdemoApplication.class, args);
+        System.out.println("Via http://127.0.0.1:8099/ to trigger this demo.");
+    }
+
+}
