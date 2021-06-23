b560b98080e460ec900d6002ee4d4ab52d1aa882
ycyoes <ycyoes@163.com>
2021-06-23 11:28:08 +0800

    classloader test

--- src/main/java/com/opensource/mybatis/Test.java
+++ src/main/java/com/opensource/mybatis/Test.java
@@ -1,4 +1,17 @@
 package com.opensource.mybatis;
 
+
 public class Test {
+
+    public static void main(String[] args) {
+       ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        System.out.println(classloader);
+        ClassLoader classloader2 = new Test().getClassLoader();
+        System.out.println(classloader2);
+
+    }
+
+    public ClassLoader getClassLoader(){
+        return getClass().getClassLoader();
+    }
 }
