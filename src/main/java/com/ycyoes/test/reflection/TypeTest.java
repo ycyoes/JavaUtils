d0a3ecf0473dd25e0f865924c2f254b17815f455
ycyoes <ycyoes@163.com>
2021-06-24 23:04:52 +0800

    getMethod

--- src/main/java/com/ycyoes/test/reflection/TypeTest.java
+++ src/main/java/com/ycyoes/test/reflection/TypeTest.java
@@ -2,11 +2,15 @@ package com.ycyoes.test.reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 
 import com.ycyoes.demos.basic.reflection.RichType;
+import com.ycyoes.test.entity.Student1;
+import com.ycyoes.test.entity.User;
+import org.apache.ibatis.reflection.TypeParameterResolver;
 
 public class TypeTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException {
 		RichType richType = getInstance(RichType.class);
 		System.out.println(richType.getRichProperty());
 		//null可直接转为类类型
@@ -26,6 +30,16 @@ public class TypeTest {
 			e.printStackTrace();
 		}
 
+		//泛型类型
+		//使用TypeParameterResolver分析User类中getInfo方法输出结果的具体类型
+		Type type = TypeParameterResolver.resolveReturnType(User.class.getMethod("getInfo"),User.class);
+		System.out.println("User类中getInfo方法的输出结果类型：\n" + type);
+
+		Type type2 = TypeParameterResolver.resolveReturnType(User.class.getMethod("getInfo"), Student1.class);
+		System.out.println("Student类中getInfo方法的输出结果类型: \n" + type2);
+
+		Method method1 = User.class.getMethod("getInfo");
+		System.out.println(method1.getDeclaringClass());
 
 	}
 	
