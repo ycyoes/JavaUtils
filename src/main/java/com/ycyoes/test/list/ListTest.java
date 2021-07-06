79932ef04b43ac2d48c53c2af051227ac75c5c17
ycyoes <ycyoes@163.com>
2021-07-06 09:36:58 +0800

    list test

--- src/main/java/com/ycyoes/test/list/ListTest.java
+++ src/main/java/com/ycyoes/test/list/ListTest.java
@@ -1,13 +1,26 @@
 package com.ycyoes.test.list;
 
+import com.ycyoes.demos.geetime.algorithm.chapter5.Array;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ListTest {
     public static void main(String[] args) {
-        int[] a = {1,2,3};
-        List list = Arrays.asList(a);
+        List list = new ArrayList();
+        System.out.println("whether is empty or not: " + list.isEmpty());
+        list.add(1);
         list.add(2);
-        System.out.println(list);
+        Arrays.asList(list).forEach(System.out::println);
+        Object a = list.remove(0);
+        System.out.println("element of deleted: " + a);
+        list.add(3);
+        list.add("test");
+        Arrays.asList(list).forEach(System.out::println);
+
+        list.remove("test");
+        Arrays.asList(list).forEach(System.out::println);
+
     }
 }
