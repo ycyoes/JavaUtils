fc1ca4bf4babc9d98af681321597ef88998799ab
ycyoes <ycyoes@163.com>
2021-07-07 11:01:37 +0800

    pattern of lazy singleton

---
+++ src/main/java/com/ycyoes/test/designpattern/singleton/LazySimpleSingleton.java
@@ -0,0 +1,14 @@
+package com.ycyoes.test.designpattern.singleton;
+
+//懒汉式单例在外部需要使用的时候才进行实例化
+public class LazySimpleSingleton {
+    private LazySimpleSingleton() {}
+    //静态块，公共内存区域
+    private static LazySimpleSingleton lazy = null;
+    public static LazySimpleSingleton getInstance() {
+        if (lazy == null) {
+            lazy = new LazySimpleSingleton();
+        }
+        return lazy;
+    }
+}
