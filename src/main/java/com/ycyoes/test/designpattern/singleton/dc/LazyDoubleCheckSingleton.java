7d0a19236f27f9402cdc40f3e455c8275c9119cc
ycyoes <ycyoes@163.com>
2021-07-07 17:12:11 +0800

    singleton of double check

---
+++ src/main/java/com/ycyoes/test/designpattern/singleton/dc/LazyDoubleCheckSingleton.java
@@ -0,0 +1,22 @@
+package com.ycyoes.test.designpattern.singleton.dc;
+
+public class LazyDoubleCheckSingleton {
+    private volatile static LazyDoubleCheckSingleton instance;
+    private LazyDoubleCheckSingleton() {}
+
+    public static LazyDoubleCheckSingleton getInstance() {
+
+        //检查是否要阻塞
+        if (instance == null) {
+            synchronized (LazyDoubleCheckSingleton.class) {
+                //检查是否要重新创建实例
+                if (instance == null) {
+                    instance = new LazyDoubleCheckSingleton();
+                    //指令重排序的问题
+                }
+            }
+        }
+
+        return instance;
+    }
+}
