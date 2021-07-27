71e0abafb3f5e930c67981d354854573199dd3bf
ycyoes <ycyoes@163.com>
2021-07-27 09:36:50 +0800

    operation of linkedlist

---
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter7/LinkListOper.java
@@ -0,0 +1,15 @@
+package com.ycyoes.demos.geetime.algorithm.chapter7;
+
+/**
+ * 1) 单链表反转
+ * 2) 链表中环的检测
+ * 3) 两个有序的链表合并
+ * 4) 删除链表倒数第n个结点
+ * 5) 求链表的中间结点
+ *
+ * @author ycyoes
+ * @date 2021-07-27 09:36
+ */
+public class LinkListOper {
+
+}
