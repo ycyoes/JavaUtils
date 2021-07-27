190f96281fc2777f69ddca8ef9db9e58efc8cf9f
ycyoes <ycyoes@163.com>
2021-07-27 15:13:31 +0800

    reverse of list

--- src/main/java/com/ycyoes/demos/geetime/algorithm/chapter7/LinkListOper.java
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter7/LinkListOper.java
@@ -12,4 +12,44 @@ package com.ycyoes.demos.geetime.algorithm.chapter7;
  */
 public class LinkListOper {
 
+    //单链表反转
+    public static Node reverse(Node list) {
+        Node current = list, prev = null;
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+
+
+    public static void printAll(Node list) {
+        Node p = list;
+        while (p != null) {
+            System.out.println(p.data + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public static Node createNode(int value) {
+        return new Node(value, null);
+    }
+
+    public static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+    }
 }
