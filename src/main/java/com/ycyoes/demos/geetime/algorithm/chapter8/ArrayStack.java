aa25be726a81d5023811abf12cb74153531d5433
ycyoes <ycyoes@163.com>
2021-07-30 11:32:30 +0800

    基于数组实现的顺序栈,出栈入栈操作

--- src/main/java/com/ycyoes/demos/geetime/algorithm/chapter8/ArrayStack.java
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter8/ArrayStack.java
@@ -19,6 +19,24 @@ public class ArrayStack {
     }
 
     //入栈操作
+    public boolean push(String item) {
+        //数组空间不够了，直接返回false,入栈失败
+        if (count == n) return false;
+        //将item放到下标为count的位置，并且count加一
+        items[count] = item;
+        ++count;
+        return true;
+    }
+
+    //出栈操作
+    public String pop() {
+        //栈为空，则直接返回null
+        if (count == 0) return null;
+        //返回下标为count-1的数组元素，并且栈中元素个数count减一
+        String tmp = items[count-1];
+        --count;
+        return tmp;
+    }
 
 
 }
