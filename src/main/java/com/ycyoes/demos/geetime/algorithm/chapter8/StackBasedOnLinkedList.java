46a984fe4a8cd5f51436fba918d9f0aebf51b618
ycyoes <ycyoes@163.com>
2021-08-17 17:53:36 +0800

    stack based on linked list

--- src/main/java/com/ycyoes/demos/geetime/algorithm/chapter8/StackBasedOnLinkedList.java
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter8/StackBasedOnLinkedList.java
@@ -1,8 +1,5 @@
 package com.ycyoes.demos.geetime.algorithm.chapter8;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * 基于链表实现的顺序栈
  *
@@ -10,17 +7,49 @@ import java.util.List;
  * @date 2021-08-17 18:23
  */
 public class StackBasedOnLinkedList {
-    List list = new LinkedList();
+    private Node top = null;
+
     //入栈操作
-    public boolean push(String item) {
-        list.add(item);
-        return true;
+    public void push(int value) {
+        Node newNode = new Node(value, null);
+        //判断是否栈空
+        if(top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
     }
 
     //出栈操作
     public Object pop() {
-        return list.get(list.size() - 1);
+        if (top == null) return -1;
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    public void printAll() {
+        Node p = top;
+        while(p != null) {
+            System.out.println(p.data + "");
+            p = p.next;
+        }
+        System.out.println();
     }
 
 
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+    }
 }
