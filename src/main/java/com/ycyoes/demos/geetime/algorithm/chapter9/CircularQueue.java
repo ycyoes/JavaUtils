c6f4231ef3abb87a4f2a478b8e0e5efd006c72c9
ycyoes <ycyoes@163.com>
2021-08-17 20:16:01 +0800

    循环队列

---
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter9/CircularQueue.java
@@ -0,0 +1,35 @@
+package com.ycyoes.demos.geetime.algorithm.chapter9;
+
+public class CircularQueue {
+    //数组：items, 数组大小n
+    private String[] items;
+    private int n = 0;
+    //head表示队头下标，tail标识队尾下标
+    private int head = 0;
+    private int tail = 0;
+
+    //申请一个大小为capacity的数组
+    public CircularQueue(int capacity) {
+        items = new String[capacity];
+        n = capacity;
+    }
+
+    //入队
+    public boolean enqueue(String item) {
+        //队列满了
+        if ((tail + 1) % n == head) return false;
+        items[tail] = item;
+        //整数x对整数y取余 得到的结果是 0~y-1之间的整数
+        tail = (tail + 1) % n;
+        return true;
+    }
+
+    //出队
+    public String dequeue() {
+        //如果head == tail 标识队列为空
+        if (head == tail) return null;
+        String item = items[head];
+        head = (head + 1) % n;
+        return item;
+    }
+}
