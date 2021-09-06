2a9821b35796a26c336a839fc377ada2df22afd9
ycyoes <ycyoes@163.com>
2021-09-06 18:11:55 +0800

    查找最后一个值等于给定值的元素

--- src/main/java/com/ycyoes/demos/geetime/algorithm/chapter16/BinarySearch.java
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter16/BinarySearch.java
@@ -1,19 +1,34 @@
 package com.ycyoes.demos.geetime.algorithm.chapter16;
-
+/**
+ * 二分查找变种
+ *
+ * @author ycyoes
+ * @date 2021-09-06
+ */
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 2};
+        int r = bsearchLast(a, 3, 2);
+        System.out.println(r);
+    }
 
-    //查找最后一个值等于给定值的元素 - 方法一
-    public int bsearchLast (int[] a, int n, int value) {
+    //查找最后一个值等于给定值的元素
+    public static int bsearchLast (int[] a, int n, int value) {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
             if (a[mid] < value) {
-
+                low = mid + 1;
+            } else if(a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid == 0) || (mid == n - 1) || (a[mid + 1] != value)) return mid;
+                else low = mid + 1;
             }
         }
 
-
+        return -1;
     }
 
     //查找第一个值等于给定值的元素 - 方法二
@@ -31,6 +46,7 @@ public class BinarySearch {
                 else high = mid - 1;
             }
         }
+        return -1;
     }
 
     //查找第一个值等于给定值的元素 - 方法一
