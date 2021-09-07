529586075bb7ccee2025f420477addddc8b8923b
ycyoes <ycyoes@163.com>
2021-09-07 09:54:14 +0800

    skip list

---
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter17/SkipList.java
@@ -0,0 +1,12 @@
+package com.ycyoes.demos.geetime.algorithm.chapter17;
+
+/**
+ * 跳表的一种实现方法。
+ * 跳表中存储的是正整数，并且存储的是不重复的。
+ *
+ * @author ycyoes
+ * @date 2021-09-07
+ */
+public class SkipList {
+
+}
