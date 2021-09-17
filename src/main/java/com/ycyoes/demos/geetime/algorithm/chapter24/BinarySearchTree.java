5fc04275666de098ce9d3ac08db3875744322f45
ycyoes <ycyoes@163.com>
2021-09-17 15:48:10 +0800

    insert of binarysearchtree

--- src/main/java/com/ycyoes/demos/geetime/algorithm/chapter24/BinarySearchTree.java
+++ src/main/java/com/ycyoes/demos/geetime/algorithm/chapter24/BinarySearchTree.java
@@ -1,4 +1,59 @@
 package com.ycyoes.demos.geetime.algorithm.chapter24;
 
+/**
+ *
+ * 二叉查找树遍历
+ *
+ * @author ycyoes
+ * @create 2021-09-17 19:56
+ *
+ */
 public class BinarySearchTree {
+    private Node tree;
+
+    public Node find(int data) {
+        Node p = tree;
+        while (p != null) {
+            if (data < p.data) p = p.left;
+            else if (data > p.data) p = p.right;
+            else return p;
+        }
+
+        return null;
+    }
+
+    public void insert(int data) {
+        if (null == tree) {
+            tree = new Node(data);
+            return;
+        }
+
+        Node p = tree;
+        while (p != null) {
+            if (data > p.data) {
+                if (p.right == null) {
+                    p.right = new Node(data);
+                    return;
+                }
+                p = p.right;
+            } else { //data < p.data
+                if (p.left == null) {
+                    p.left = new Node(data);
+                    return;
+                }
+                p = p.left;
+            }
+        }
+    }
+
+
+    public static class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
 }
