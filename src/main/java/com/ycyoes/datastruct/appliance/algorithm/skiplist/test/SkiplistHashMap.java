49ad35bc2b431162812fafd49c08adbcfe4c04b2
ycyoes <ycyoes@163.com>
2021-09-23 18:59:08 +0800

    find val

--- src/main/java/com/ycyoes/datastruct/appliance/algorithm/skiplist/test/SkiplistHashMap.java
+++ src/main/java/com/ycyoes/datastruct/appliance/algorithm/skiplist/test/SkiplistHashMap.java
@@ -10,13 +10,40 @@ package com.ycyoes.datastruct.appliance.algorithm.skiplist.test;
  */
 public class SkiplistHashMap {
 
+
+    static class SkiplistNode<K extends Comparable<K>, V> {
+        int hash;
+        K key;
+        V value;
+        int maxLevel;
+        Node<K, V>[] nexts;
+
+        public V findValue(K key) {
+            int level = this.maxLevel;
+            SkiplistNode<K, V> q = this;
+            int c;
+            //i-- 控制向下
+            for (int i = (level - 1); i >= 0; i--) {
+                while (q.nexts[i] != null && (c = q.nexts[i].key.compareTo(key)) <= 0) {
+                    if (c == 0) {
+                        //找到了返回
+                        return q.nexts[i].value;
+                    }
+                    //控制向右
+                    q = q.nexts[i];
+                }
+            }
+            return null;
+        }
+    }
+
     /**
      * 链表节点
      *
      * @param <K>
      * @param <V>
      */
-    static class Node<K extends Comparable<K>, V> {
+    static class Node<K extends Comparable<K>, V> extends SkiplistNode<K, V> {
         int hash;
         K key;
         V value;
