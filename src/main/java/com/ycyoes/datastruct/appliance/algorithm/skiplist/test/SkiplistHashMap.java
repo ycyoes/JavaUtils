f5e33f08f57f3f4b223a1d3196036b6c37afefb9
ycyoes <ycyoes@163.com>
2021-09-23 17:05:04 +0800

    Node

--- src/main/java/com/ycyoes/datastruct/appliance/algorithm/skiplist/test/SkiplistHashMap.java
+++ src/main/java/com/ycyoes/datastruct/appliance/algorithm/skiplist/test/SkiplistHashMap.java
@@ -9,4 +9,24 @@ package com.ycyoes.datastruct.appliance.algorithm.skiplist.test;
  *
  */
 public class SkiplistHashMap {
+
+    /**
+     * 链表节点
+     *
+     * @param <K>
+     * @param <V>
+     */
+    static class Node<K extends Comparable<K>, V> {
+        int hash;
+        K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
