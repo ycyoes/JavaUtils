b44054fc91b5718d6b68c07c8314214cb2e2e2c2
ycyoes <ycyoes@163.com>
2021-10-15 16:35:42 +0800

    类型擦除

---
+++ src/main/java/com/ycyoes/test/Generic.java
@@ -0,0 +1,15 @@
+package com.ycyoes.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Generic {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        //类型擦除
+        //可通过反射添加其他类型元素，
+        list.getClass().getMethod("add", Object.class).invoke(list, "asd");
+        Arrays.asList(list).forEach(System.out::println);
+    }
+}
