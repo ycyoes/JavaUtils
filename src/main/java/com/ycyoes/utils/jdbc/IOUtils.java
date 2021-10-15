e7a6b62760380b88cd273bb7e926741e61339f7e
ycyoes <ycyoes@163.com>
2021-10-15 11:27:28 +0800

    关闭实现closable,AutoClosable接口的类

---
+++ src/main/java/com/ycyoes/utils/jdbc/IOUtils.java
@@ -0,0 +1,27 @@
+package com.ycyoes.utils.jdbc;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public abstract class IOUtils {
+
+    public static void closeQuietly(Closeable closeable) {
+        if(closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeQuietly(AutoCloseable closeable) {
+        if(closeable != null) {
+            try {
+                closeable.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
