14571fb8e23c9d25766c3b9bec54bbc954c110dc
ycyoes <ycyoes@163.com>
2021-10-18 21:29:03 +0800

    spi test

--- src/main/java/com/ycyoes/test/spi/DatabaseSearch.java
+++ src/main/java/com/ycyoes/test/spi/DatabaseSearch.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class DatabaseSearch implements Search {
     @Override
     public List<String> searchDoc(String keyWord) {
-        System.out.println("数据搜索： " + keyWord);
+        System.out.println("数据搜索：" + keyWord);
         return null;
     }
 }
