945e22cea22d7b2684d688ccb10c3d4c88ff7422
ycyoes <ycyoes@163.com>
2021-11-03 16:24:06 +0800

    minio demo

---
+++ src/main/java/com/ycyoes/test/minio/FileUploader.java
@@ -0,0 +1,45 @@
+package com.ycyoes.test.minio;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
+import io.minio.errors.MinioException;
+
+public class FileUploader {
+    public static void main(String[] args) throws Exception {
+        try {
+            // Create a minioClient with the MinIO server playground, its access key and secret key.
+            MinioClient minioClient =
+                    MinioClient.builder()
+                            .endpoint("http://172.16.100.236:9000")
+                            .credentials("minioadmin", "minioadmin")
+                            .build();
+
+            // Make 'asiatrip' bucket if not exist.
+            boolean found =
+                    minioClient.bucketExists(BucketExistsArgs.builder().bucket("bsc").build());
+            if (!found) {
+                // Make a new bucket called 'asiatrip'.
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket("bsc").build());
+            } else {
+                System.out.println("Bucket 'asiatrip' already exists.");
+            }
+
+            // Upload '/home/user/Photos/asiaphotos.zip' as object name 'asiaphotos-2015.zip' to bucket
+            // 'asiatrip'.
+            minioClient.uploadObject(
+                    UploadObjectArgs.builder()
+                            .bucket("bsc")
+                            .object("1.jpg")
+                            .filename("e:/1.jpg")
+                            .build());
+            System.out.println(
+                    "'e:/1.jpg' is successfully uploaded as "
+                            + "object '1.jpg' to bucket 'asiatrip'.");
+        } catch (MinioException e) {
+            System.out.println("Error occurred: " + e);
+//            System.out.println("HTTP trace: " + e.httpTrace());
+        }
+    }
+    }
