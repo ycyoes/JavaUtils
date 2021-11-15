d45046bec6d071d513bfb5890d67e47bc8c004f4
ycyoes <ycyoes@163.com>
2021-11-15 16:37:54 +0800

    kafka consumer

---
+++ src/main/java/com/kafka/demo/ConsumerMain.java
@@ -0,0 +1,42 @@
+package com.kafka.demo;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerMain {
+
+    public static void main(String[] args) {
+        Consumer<String, String> consumer = createConsumer();
+        consumer.subscribe(Collections.singleton("TestTopic"));
+        while (true) {
+            ConsumerRecords records = consumer.poll(Duration.ofSeconds(10));
+            records.forEach(new java.util.function.Consumer<ConsumerRecord>() {
+                @Override
+                public void accept(ConsumerRecord record) {
+                    System.out.println(record.key() + "\t" + record.value());
+                }
+            });
+        }
+    }
+    private static Consumer<String, String> createConsumer() {
+        //设置Producer属性
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "192.168.253.128:9092");    //设置Broker的地址
+        properties.put("group.id", "demo-consumer-group");    //消费者分组
+        properties.put("auto.offset.reset", "earliest");    //设置消费者分组最初的消费进度为earliest
+        properties.put("enable.auto.commit", true);   //是否自动提交消费进度
+        properties.put("auto.commit.interval.ms", "1000");   //自动提交消费进度频率
+        properties.put("key.deserializer", StringSerializer.class.getName()); //消息的key的反序列化方式
+        properties.put("value.deserializer", StringSerializer.class.getName());   //消息的value的反序列化方式
+
+        //创建KafkaProducer对象
+        return new KafkaConsumer<String, String>(properties);
+    }
+}
