6d382954527ad41024a0e56662798f881e9b35a8
ycyoes <ycyoes@163.com>
2021-11-24 16:24:58 +0800

    branch handle

---
+++ src/main/java/com/ycyoes/test/functional/finterface/BranchHandle.java
@@ -0,0 +1,13 @@
+package com.ycyoes.test.functional.finterface;
+
+@FunctionalInterface
+public interface BranchHandle {
+
+    /**
+     * 分支操作
+     *
+     * @param trueHandle    为true时要进行的操作
+     * @param falseHandle   为false时要进行的操作
+     */
+    void trueOrFalse(Runnable trueHandle, Runnable falseHandle);
+}
