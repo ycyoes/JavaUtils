8b5e8f5eb3d13e7b01bbc7cc4253b91017d9e088
ycyoes <ycyoes@163.com>
2022-04-20 22:12:38 +0800

    快速实现一个缓存

---
+++ src/main/java/com/ycyoes/demos/geetime/concurrent/practise/chapter17/Cache.java
@@ -0,0 +1,9 @@
+package com.ycyoes.demos.geetime.concurrent.practise.chapter17;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cache<K, V> {
+    final Map<K, V> m = new HashMap<>();
+
+}
