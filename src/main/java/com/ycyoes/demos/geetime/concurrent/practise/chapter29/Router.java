1e314422a5e53f34a213eefded815a588b31141e
ycyoes <ycyoes@163.com>
2022-05-10 12:45:47 +0800

    Router

---
+++ src/main/java/com/ycyoes/demos/geetime/concurrent/practise/chapter29/Router.java
@@ -0,0 +1,29 @@
+package com.ycyoes.demos.geetime.concurrent.practise.chapter29;
+
+import java.util.Objects;
+
+public final class Router {
+    private final String ip;
+    private final Integer port;
+    private final String iface;
+
+    public Router(String ip, Integer port, String iface) {
+        this.ip = ip;
+        this.port = port;
+        this.iface = iface;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Router) {
+            Router r = (Router)o;
+            return iface.equals(r.iface) && ip.equals(r.ip) && port.equals(r.port);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip, port, iface);
+    }
+}
