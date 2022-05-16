f36fc6db7ea2eab85c2e0abcb9f3bcf919f866f1
ycyoes <ycyoes@163.com>
2022-05-16 11:12:05 +0800

    DeadLockTest

---
+++ src/main/java/com/ycyoes/demos/geetime/concurrent/practise/chapter34/DeadLockTest.java
@@ -0,0 +1,39 @@
+package com.ycyoes.demos.geetime.concurrent.practise.chapter34;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DeadLockTest {
+    public static void main(String[] args) throws Exception {
+        //L1/L2阶段共用的线程池
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        //L1阶段的闭锁
+        CountDownLatch l1 = new CountDownLatch(2);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("L1");
+            //执行L1阶段任务
+            es.execute(() -> {
+                //L2阶段的闭锁
+                CountDownLatch l2 = new CountDownLatch(2);
+                //执行L2阶段子任务
+                for (int j = 0; j < 2; j++) {
+                    es.execute(() -> {
+                        System.out.println("L2");
+                        l2.countDown();
+                    });
+                }
+                //等待L2阶段任务执行完
+                try {
+                    l2.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                l1.countDown();
+            });
+        }
+        //等L1阶段任务执行完
+        l1.await();
+        System.out.println("end");
+    }
+}
