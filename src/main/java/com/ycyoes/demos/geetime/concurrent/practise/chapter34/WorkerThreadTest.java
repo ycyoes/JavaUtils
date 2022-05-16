009c93f2c38928b4c45d93464ed8a1ad74d779b6
ycyoes <ycyoes@163.com>
2022-05-16 10:34:16 +0800

    用线程池实现的 echo 服务端

---
+++ src/main/java/com/ycyoes/demos/geetime/concurrent/practise/chapter34/WorkerThreadTest.java
@@ -0,0 +1,60 @@
+package com.ycyoes.demos.geetime.concurrent.practise.chapter34;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * echo 的实现
+ *
+ * @author ycyoes
+ * @date 2022-05-16 21:39
+ */
+public class WorkerThreadTest {
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(500);
+        final ServerSocketChannel ssc = ServerSocketChannel.open().bind(new InetSocketAddress(8080));
+        //处理请求
+        try {
+            while (true) {
+                //接收请求
+                SocketChannel sc = ssc.accept();
+                //将请求处理任务提交给线程池
+                es.execute(() -> {
+                    try {
+                        //读Socket
+                        ByteBuffer rb = ByteBuffer.allocate(1024);
+                        sc.read(rb);
+                        //模拟处理请求
+                        Thread.sleep(200);
+
+                        //写Socket
+                        ByteBuffer wb = (ByteBuffer) rb.flip();
+                        sc.write(wb);
+
+                        //关闭Socket
+                        sc.close();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ssc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            es.shutdown();
+        }
+    }
+}
